b578037fb97a09baf98bf4307053c0c350d159ae
Tommy Newbold <tommynewbold@googlemail.com>
2015-12-15 22:16:06 +0000

    Expiry date added

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,8 @@
 module ItemsHelper
-end
+  include ActionView::Helpers::DateHelper
+
+  def time_left(item)
+      "#{distance_of_time_in_words(7.days.ago, item.created_at, include_seconds: true)} left"
+    end
+  end
+
